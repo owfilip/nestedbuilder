1e9c95242039aeeb8f6f70944702be0cd5583a9c
owfilip <filipowczarzak@Filips-MacBook-Pro.local>
2023-03-27 16:44:00 +0200

    updated package name. Added README.md

--- NestedBuilder.java
+++ NestedBuilder.java
@@ -1,4 +1,4 @@
-package interview;
+package org.patterns;
 
 import java.util.ArrayList;
 import java.util.List;
---
+++ README.md
@@ -0,0 +1,2 @@
+javac -d classes NestedBuilder.java
+java -cp classes org.patterns.NestedBuilder
